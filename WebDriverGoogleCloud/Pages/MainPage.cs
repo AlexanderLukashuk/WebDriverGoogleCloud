98ab6ebc1667ff071af78c89c00464900b3ee614
AlexanderLukashuk <upiter465@gmail.com>
2023-12-09 02:36:11 +0600

    Creaed main page with search method

---
+++ WebDriverGoogleCloud/Pages/MainPage.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace WebDriverGoogleCloud.Pages
+{
+    public class MainPage : BasePage
+    {
+        public MainPage(IWebDriver driver) : base(driver) {}
+
+        public IWebElement SearchIcon => WebDriver.FindElement(By.XPath("//*[@id=\"kO001e\"]/div[2]/div[1]/div/div[2]/div[2]/div[1]/form/div/input"));
+
+        public IWebElement SearchField => WebDriver.FindElement(By.XPath("//*[@id=\"kO001e\"]/div[2]/div[1]/div/div[2]/div[2]/div[1]/form/div/input"));
+
+        public void Search(string searchString)
+        {
+            SearchIcon.Click();
+
+            SearchField.SendKeys("Google Cloud Platform Pricing Calculator");
+
+            SearchField.SendKeys(Keys.Enter);
+        }
+    }
+}
