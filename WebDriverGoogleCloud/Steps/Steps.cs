86dd1f3e133dd5125c001bb4a35f583de5fae2da
AlexanderLukashuk <upiter465@gmail.com>
2023-12-09 13:24:08 +0600

    Created Steps

---
+++ WebDriverGoogleCloud/Steps/Steps.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using WebDriverGoogleCloud.Driver;
+using WebDriverGoogleCloud.Pages;
+
+namespace WebDriverGoogleCloud.Steps
+{
+    public class Steps
+    {
+        IWebDriver webDriver = null!;
+
+        MainPage mainPage = null!;
+
+        SearchResultPage searchResultPage = null!;
+
+        CalculatorPage calculatorPage = null!;
+
+        EmailPage emailPage = null!;
+
+        public void InitBrowser()
+        {
+            webDriver = WebDriverManager.Driver;
+            mainPage = new MainPage(webDriver);
+            searchResultPage = new SearchResultPage(webDriver);
+            calculatorPage = new CalculatorPage(webDriver);
+            emailPage = new EmailPage(webDriver);
+        }
+
+        public void CloseBrowser()
+        {
+            WebDriverManager.QuitDriver();
+        }
+
+        public void SearchGoogleCloud(string searchText)
+        {
+            mainPage.OpenPage();
+            mainPage.Search(searchText);
+        }
+
+        public void OpenSearchResult()
+        {
+            searchResultPage.OpenSearchResult();
+        }
+
+        public void CalculateEstimate()
+        {
+            calculatorPage.CalculateEstimate();
+        }
+
+        public void GenerateEmail()
+        {
+            emailPage.GenerateEmail();
+        }
+
+        public void SendEmail()
+        {
+            calculatorPage.SendEmail();
+            emailPage.CheckEmail();
+        }
+    }
+}
